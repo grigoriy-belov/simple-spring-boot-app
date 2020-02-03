0fba00e5eff7406b2154bad1de894a7c64d1b4d8
Grigoriy Belov <grigoriy.belov1@gmail.com>
2020-02-03 17:39:03 +0200

    Initial commit

---
+++ src/main/java/com/belov/persistence/repo/BookRepository.java
@@ -0,0 +1,12 @@
+package com.belov.persistence.repo;
+
+import com.belov.persistence.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findByTitle(String title);
+
+}
